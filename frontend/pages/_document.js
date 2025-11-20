a5a1e4c9c0ee7326f91f27fecc454f15b8a8d9e5
siteget1234-del <siteget1234@gmail.com>
2025-11-21 01:28:48 +0530

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/public/favicon.png" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
